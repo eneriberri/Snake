43bd826c5c0aaf32aae8fc8d8ae25211e5538920
Irene Nguyen <irene.m.nguyen@gmail.com>
2013-12-14 14:53:06 -0600

    binding key events

--- snake_ui.js
+++ snake_ui.js
@@ -3,18 +3,28 @@
   
   var View = SnakeGame.View = function(el) {
     this.$el = el;
+    this.board = null;
   };
   
   View.DIMENSIONS = 500;
   
   View.prototype.start = function() {
-    var grid = new Board(View.DIMENSIONS);
+    this.board = new Board(View.DIMENSIONS);
     this.$el.on('keydown');
   };
   
   View.prototype.handleKeyEvent(event) {
-    
+    var val = event.keyCode;
+    if(val === 37) //left arrow
   };
+  
+  View.KEYCODES = {
+    37: 
+    37      37      37      37      37      Left arrow
+     38      38      38      38      38      Up arrow
+     39      39      39      39      39      Right arrow
+     40      40      40      40      40      Down arrow
+  }
   
   
   
